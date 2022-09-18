f2b34e74dcf7764ec87fe1b36158c0dd61ea26cf
undazh <undazurevska@gmail.com>
2022-09-18 18:15:59 +0000

    API atslēgas pievienošana

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "rU8IFKPk1jXKQaqcE4Yjpabv1SeuZnQPEL77MPQ3"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
